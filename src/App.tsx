747f19545cda1792d172f8daceabdd6510c57aea
Juan <aragonlopezjuan87@gmail.com>
2020-05-11 17:39:07 +0200

    hover h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Header from "./components/header/header";
 import ContainerShowJob from "./components/containerShowJob/containerShowJob";
-import CardJob from "./components/cardJob/cardJob";
 import "./app.scss";
 
 function App() {
   return (
     <>
-      <div className="wrap">
-        <CardJob />
-      </div>
       <Header />
       <ContainerShowJob />
     </>
--- src/components/containerShowJob/containerShowJob.tsx
+++ src/components/containerShowJob/containerShowJob.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./containerShowJob.scss";
 
+import CardJob from "../../components/cardJob/cardJob";
+
 const ContainerShowJob = () => {
-  return <section className="container-Show-job"></section>;
+  return (
+    <section className="container-Show-job">
+      <div className="wrap">
+        <CardJob />
+        <CardJob />
+        <CardJob />
+      </div>
+    </section>
+  );
 };
 
 export default ContainerShowJob;
